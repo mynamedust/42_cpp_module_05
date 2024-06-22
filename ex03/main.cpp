27072263ff0d88ec0e4966244aef4e6c9935cfb1
mynamedust <mynamedust@gmail.com>
2024-06-22 20:27:25 +0400

    add ex03 main file

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,12 +6,26 @@ int main() {
 	AForm* robot;
 	AForm* shrubbery;
 	AForm* presidental;
+	Bureaucrat buro1("Solomon", 22);
+
 	robot = someRandomIntern.makeForm("robotomy", "Bender");
 	shrubbery = someRandomIntern.makeForm("shrubbery", "Diego");
 	presidental = someRandomIntern.makeForm("presidential", "Donald");
 	
+	buro1.signForm(*robot);
+	buro1.executeForm(*robot);
+
+	buro1.signForm(*shrubbery);
+	buro1.executeForm(*shrubbery);
+
+	buro1.signForm(*presidental);
+	buro1.executeForm(*presidental);
+
 	std::cout << *robot << std::endl;
 	std::cout << *shrubbery << std::endl;
 	std::cout << *presidental << std::endl;
-	robot
+
+	delete robot;
+	delete shrubbery;
+	delete presidental;
 }
